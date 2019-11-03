47c1d22d5d494e0f69855e7a008e44665114479b
Chris Padilla <cdpadilla42@gmail.com>
2019-11-03 14:16:17 -0600

    Add the generation of divs and the adjustment of grid template to flexible dimensions.

--- script.js
+++ script.js
@@ -29,7 +29,15 @@ function generateDivs(num) {
   // SOMETHING WEIRD! When you try to finish this code, it cancels out ALL OTHER JS! Why is this?
   // does it have to do with the way variables are used?
   // does it have to do with the order of your code? Experiment with both.
+  for (var j = 1; j < (squared + 1); j++) {
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('class', "box " + j);
+    container.appendChild(newDiv);
+  } 
+  container.classList.remove("grid-container");
+  container.setAttribute("style", "display: grid; grid-template: repeat(" + n + ", 1fr) / repeat(" + n + ", 1fr);width: 500px;height: 500px;border: 3px solid green");
 }
+
 
 // create event listener
 var boxes = document.querySelectorAll('.box')
